999292d66d10b743a4b788969d42addfb90a9202
Keaton Nordby <keatonnordby4@gmail.com>
2026-06-29 18:17:39 -0500

    sign up logic

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,52 @@
+import { User } from "../models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      message: "User created",
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ message: "Email or password is incorrect" });
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        process.env.JWT_SECRET
+    })
+  } catch (error) {}
+};
